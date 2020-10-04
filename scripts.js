715e22d96085a7ea998bf1858a95cb8146287a7f
NualiRamazan <65036368+NualiRamazan@users.noreply.github.com>
2020-10-04 23:09:11 +0600

    Edit script

--- scripts.js
+++ scripts.js
@@ -1,27 +1,25 @@
 $(document).ready(function(){
-    $(".add").click(function(){
-        $("#incomplete-tasks")
-        .append("<li><label>" + $("#new-task").val() + 
-        "</label><button class='completed'>Completed</button><button class='delete'>Delete</button></li>");
-        document.getElementById("new-task").value="";
-        delElement();
-        delCompl();
-    }), delElement(), delCompl()
-});
-
-var delElement=function() {
-    $(".delete").click(function(){
-        var isDel = confirm("Подтверждение удаления")
-        if (isDel) {
-            $(this.parentElement).remove();
+    $('input').keydown(function(e) {
+        if(e.keyCode === 13) {
+            addElement();
         }
     });
-}
+}), $(document).on('click', '.add', function() {
+    addElement();
+}), $(document).on('click', '.delete', function() {
+    var isDel = confirm("Подтверждение удаления")
+    if (isDel) {
+        $(this.parentElement).remove();
+    }
+}), $(document).on('click', '.completed', function() {
+    $("#completed-tasks").append(this.parentElement);
+    this.remove();
+});
 
-var delCompl=function() {
-    $(".completed").click(function(){
-        $("#completed-tasks")
-        .append(this.parentElement);
-        this.remove()
-    })
+var addElement=function() {
+    $("#incomplete-tasks").append(
+        "<li><label>" + $("#new-task").val() + 
+        "</label><button class='completed'>Completed</button>" +
+        "<button class='delete'>Delete</button></li>");
+    document.getElementById("new-task").value="";
 }
